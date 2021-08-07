ad1b74bf85539d4f5d6ce02d5790f79b5fcf0397
manuelruizredondo <manuelruizredondo@gmail.com>
2021-08-07 19:55:22 +0200

    Cambios en website

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,36 @@
 
 function initSwiper() {
-	var mySwiper = new Swiper(".swiper-container", {
-		// esto es opcional
-		direction: "horizontal",
-		slidesPerView: 4,
 
+
+
+	var mySwiper = new Swiper(".swiper-container", {
+        slidesPerView: 5,
+        spaceBetween: 4,
 		loop: true,
-		spaceBetween: 5,
-		//visibilityFullFit: true,
-		pagination: ".swiper-pagination",
-		paginationClickable: true,
-		nextButton: ".swiper-button-next",
-		prevButton: ".swiper-button-prev",
-	});
+
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 4,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 8,
+          },
+        },
+      });
+    
+
+
+
 
 
 	var mySwiper2 = new Swiper(".swiper-container2", {
