898272915318c58b6b865b3bba5eaf789835b1ad
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2022-01-21 01:29:35 +0500

    inner blog page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-exports.createPages = async ({ actions }) => {
+const path = require(`path`)
+
+module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const blogTemplate = path.resolve(`./src/templates/blog.js`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(filter: { frontmatter: { page: { ne: true } } }) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.frontmatter.path}`,
+
+      context: {
+        slug: edge.node.frontmatter.path,
+      }, // additional data can be passed via context
+    })
   })
 }
